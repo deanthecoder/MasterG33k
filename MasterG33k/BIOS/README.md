46aa80a151351cb9b84fe458e8d52e3d064d73eb
Dean Edis <dean.edis@gmail.com>
2026-01-11 16:49:54 +0000

    Feature: Conditionally prompt user to install system BIOS.

---
+++ MasterG33k/BIOS/README.md
@@ -0,0 +1,5 @@
+# Master System BIOS
+
+Copy your Sega Master System BIOS ROM file into this folder.
+
+Supported file extension: `.sms`
--- MasterG33k/Views/App.axaml.cs
+++ MasterG33k/Views/App.axaml.cs
@@ -8,9 +8,15 @@
 //
 // THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND.
 
+using System;
+using System.IO;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using CSharp.Core.UI;
+using DialogHostAvalonia;
+using Material.Icons;
 using MasterG33k.ViewModels;
 
 namespace MasterG33k.Views;
@@ -32,18 +38,37 @@ public class App : Application
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             var viewModel = new MainWindowViewModel();
-            desktop.MainWindow = new MainWindow
+            var mainWindow = new MainWindow
             {
                 DataContext = viewModel
             };
+            desktop.MainWindow = mainWindow;
 
             desktop.MainWindow.Closed += (_, _) =>
             {
                 viewModel.Dispose();
                 Settings.Instance.Dispose();
             };
+            mainWindow.Opened += (_, _) => EnsureBiosAvailable(desktop);
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static void EnsureBiosAvailable(IClassicDesktopStyleApplicationLifetime desktop)
+    {
+        var biosFolder = Path.Combine(AppContext.BaseDirectory, "BIOS");
+        var hasBios = Directory.Exists(biosFolder) &&
+                      Directory.EnumerateFiles(biosFolder, "*.sms").Any();
+        if (hasBios)
+            return;
+
+        DialogHost.Show(new MessageDialog
+            {
+                Message = "No Master System BIOS ROM found.",
+                Detail = $"Copy your Sega Master System BIOS ROM (.sms) into:\n{biosFolder}",
+                Icon = MaterialIconKind.AlertCircleOutline
+            },
+            (DialogClosingEventHandler)((_, _) => desktop.Shutdown()));
     }
 }
